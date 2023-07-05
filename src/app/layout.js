4d9c6efda5009d907b24fcd2f829fd09b15bfaa0
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-07-05 17:55:04 +0100

    Fix: titulo

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Esmeraldo Alberto-FullStack Developer',
+  title: 'Esmeraldo Alberto - FullStack Developer',
   description: 'Esmeraldo Alberto Web Portfolio'
 };
 
