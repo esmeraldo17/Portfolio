e644005d834bc7b8bd66e7bb1b45c9ad080521d0
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-07-05 20:01:10 +0100

    feat: criando componetes projects e Contact

---
+++ src/app/Components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div id='about' className='ml-[220px] bg-cyan-500 h-[100vh] p-6'>
+      <h1 className='text-4xl font-semibold flex flex-col items-center'>Projetos</h1>
+    </div>
+  )
+}
+
+export default Projects
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import About from './Components/About';
+import Contact from './Components/Contact';
 import Layout from './Components/Layout';
+import Projects from './Components/Projects';
 import Welcome from './Components/Welcome';
 
 export default function Home() {
   return (
     <Layout>
       <Welcome/>
+      <Projects/>
       <About />
+      <Contact/>
     </ Layout>
   );
 }
