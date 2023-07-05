30203cc5b8a36909286002b8514ba9c3900cac50
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-07-05 17:50:22 +0100

    feat: adicionando titulo

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Portfolio',
+  title: 'Esmeraldo Alberto Full-Stack Developer',
   description: 'Esmeraldo Alberto Web Portfolio'
 };
 
