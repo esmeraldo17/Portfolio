c65f2005449e23e92b5a08a62b5672498b910faf
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-07-06 23:03:45 +0100

    chore: populando os projetos

---
+++ src/app/assets/data/data.js
@@ -0,0 +1,21 @@
+import trecipes from '../images/trecipes.png';
+
+const data = [
+    {
+    name: 'T-Recepies',
+    descripton: 'Aplicativo de receitas comidas e bebidas',
+    image: trecipes,
+    technologies: ['Docker', 'React'],
+    github: 'https://github.com/esmeraldo17/T-Recepies',
+    deploy: 'https://lnkd.in/diPPH6ET'
+    },
+    {
+    name: 'Geek Drinks',
+    descripton: 'Plataforma de delivery de cerveja',
+    image: trecipes,
+    technologies: ['Docker', 'React', 'Node', 'Express', 'Mysql'],
+    github: 'https://github.com/esmeraldo17/delivery-app',
+    },
+  ]
+
+export default  data;
