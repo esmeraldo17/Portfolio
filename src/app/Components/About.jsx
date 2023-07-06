ec2e44a1b1540ee3a125636398602f2bdbf68910
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-07-06 21:37:10 +0100

    fix: ajustes de tamanho no sobremim

--- src/app/Components/About.jsx
+++ src/app/Components/About.jsx
@@ -11,16 +11,14 @@ const About = () => {
         {/* <span className='font-semibold'></span> */}
           <span className='font-semibold'>Olá!</span> Sou um 
           <span className='font-semibold'> desenvolvedor Web FullStack </span> 
-          com paixão por criar soluções inovadoras e cativantes. 
-          Com um ano e dois meses de experiência como estudante e em projetos freelance, 
-          tenho me dedicado a aprimorar minhas habilidades e explorar os desafios fascinantes do mundo digital.<br/>
+          com paixão por criar soluções inovadoras e cativantes. Com 
+          <span className='font-semibold'> um ano e dois meses de experiência como estudante e em projetos freelance.</span>
+          <br/>
           Durante minha jornada como desenvolvedor, estudei conceito importantes da programação como: 
           <span className='font-semibold'> Algoritmos, Design Patterns, Complexibilidade de Codigo(Big-O), Extrutura de Dados, </span>
-          e mergulhei em várias tecnologias,  incluindo 
-          <span className='font-semibold'> HTML, CSS, JavaScript </span>
-          e frameworks populares como 
+          e mergulhei em várias tecnologias e frameworks populares como 
           <span className='font-semibold'> React, Next e Node.js. </span>
-          Tenho um bom domínio do desenvolvimento 
+          Domino o desenvolvimento 
           <span className='font-semibold'> front-end, </span>
           criando interfaces 
           <span className='font-semibold'> intuitivas e responsivas </span> 
@@ -50,7 +48,7 @@ const About = () => {
           <span className='font-semibold'> funcionalidade e design elegante.</span>
         </p>
       </div>
-      <Image src={image}  alt='thumb' className='rounded-tl-lg rounded-tr-md rounded-br-lg rounded-bl-sm opacity-80'></Image>
+      <Image src={image}  alt='thumb' className='rounded-tl-lg rounded-tr-md rounded-br-lg rounded-bl-sm w-[30%] opacity-80'></Image>
     </div>
   );
 };
