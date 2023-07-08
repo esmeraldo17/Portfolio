8ebf9eddcd7367e85db438a89e22d685cd9e86c7
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-07-09 00:03:12 +0100

    feat: Implementacao do Google analitics

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,18 @@ import Contact from './Components/Contact';
 import Layout from './Components/Layout';
 import Projects from './Components/Projects';
 import Welcome from './Components/Welcome';
+import GoogleAnalytics from '@bradgarropy/next-google-analytics';
 
 export default function Home() {
   return (
-    <Layout>
-      <Welcome />
-      <Projects />
-      <About />
-      <Contact />
-    </ Layout>
+    <>
+      <GoogleAnalytics measurementId='G-QW92SHWWYM'/>
+      <Layout>
+        <Welcome />
+        <Projects />
+        <About />
+        <Contact />
+      </ Layout>
+    </>
   );
 }
